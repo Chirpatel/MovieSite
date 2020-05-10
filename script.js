b166cfab62ab6f7459013e7bffe73190a5ce35fb
Chirpatel <chirpatelss@gmail.com>
2020-05-11 02:37:21 +0530

    Read more option

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ var elem = document.getElementById('input');
 
 
 
-var url=`https://api.themoviedb.org/3/trending/all/day?api_key=${api_key}`
+var url;
 function trending(){
-    //console.log("Trending");
+    window.removeEventListener('hashchange', hashHandler, false);
+    url=`https://api.themoviedb.org/3/trending/all/day?api_key=${api_key}`
+    document.getElementById('name').innerText=`Trending`
+    console.log("Trending");
+    elem.innerHTML=""
     axios.get(url)
     .then(function(response){
-        //console.log(response);
+        console.log(response);
         //console.log(response.data.results.length)
         for(let i=0;i<response.data.results.length;i++){
             var d  = document.createElement('div');
@@ -21,30 +25,36 @@ function trending(){
             //console.log(title)
             if(title==undefined)
              title =response.data.results[i].name;
-            
-            d.innerHTML=`<div class="card" >
-            <img class="card-img-top" src="https://image.tmdb.org/t/p/w780/${response.data.results[i].poster_path}" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">${title}</h5>
-              <p class="card-text">${response.data.results[i].overview}</p>
-              <!--<a href="#" class="btn btn-primary">Go somewhere</a>
-            --></div>`
-
-            elem.appendChild(d);
+             var id=response.data.results[i].id;
+             var type= response.data.results[i].media_type;
+             d.innerHTML=`<div class="card" onmouseover="display(${id})" onmouseout="displayhide(${response.data.results[i].id})">
+             <img class="card-img-top" src="https://image.tmdb.org/t/p/w780/${response.data.results[i].poster_path}" alt="Card image cap">
+             <div class="card-body">
+               <h5 class="card-title">${title}</h5>
+               <p class="card-text">${response.data.results[i].overview.substring(0, 100)}...</p>
+               <div class="fixed-bottom"><a href="#"  id="read${id}"class="btn read btn-primary " onclick="details('${id}','${type}')">Read More</a></div></div>`
+             elem.appendChild(d);
         }
     })
 }
 
+function display(id){
+    document.getElementById(`read${id}`).style.visibility="visible";
+}
+function displayhide(id){
+    document.getElementById(`read${id}`).style.visibility="hidden";
+}
+
 
 url1=`https://api.themoviedb.org/3/search/multi`
+var string=""
 
-
-function search(){
+async function search(){
     elem.innerHTML=""
-    var string = document.getElementById('search').value;
+    string = document.getElementById('search').value;
     //console.log("Searching");
     document.getElementById('name').innerText=`Search result: "${string}"`
-    axios.get(url1,{
+    await axios.get(url1,{
         params:{
             api_key:api_key,
             language:"en-US",
@@ -59,6 +69,7 @@ function search(){
             searchpage(i,string);
         }
     })
+    window.addEventListener('hashchange', hashHandler, true);
 }
 function searchpage(page,string){
     var string = document.getElementById('search').value;
@@ -74,7 +85,7 @@ function searchpage(page,string){
         }
     })
     .then(function(response){
-        //console.log(response);
+        console.log(response);
         //console.log(response.data.results.length)
         for(let i=0;i<response.data.results.length;i++){
             if(response.data.results[i].poster_path!=null){
@@ -87,15 +98,65 @@ function searchpage(page,string){
             //console.log(title)
             if(title==undefined)
              title =response.data.results[i].name;
-            d.innerHTML=`<div class="card" >
+             var id=response.data.results[i].id;
+             var type= response.data.results[i].media_type;
+            d.innerHTML=`<div class="card" onmouseover="display(${id})" onmouseout="displayhide(${response.data.results[i].id})">
             <img class="card-img-top" src="https://image.tmdb.org/t/p/w780/${response.data.results[i].poster_path}" alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">${title}</h5>
-              <p class="card-text">${response.data.results[i].overview}</p>
-              <!--<a href="#" class="btn btn-primary">Go somewhere</a>
-            --></div>`
+              <p class="card-text">${response.data.results[i].overview.substring(0, 100)}...</p>
+              <div class="fixed-bottom"><a href="#"  id="read${id}"class="btn read btn-primary " onclick="details('${id}','${type}')">Read More</a></div></div>`
             
-            elem.appendChild(d);}
+            elem.appendChild(d);
+
+            }
         }
     })
+}
+
+
+async function details(id,type){
+    console.log(id);
+    elem.innerHTML=""
+    document.getElementById('name').innerText=""
+    var data;
+    url=`https://api.themoviedb.org/3/${type}/${id}/external_ids`
+    
+    await axios.get(url,{
+        params:{
+            api_key:api_key,
+        }
+    })
+    .then(function(response){
+        console.log(response);
+        data=response;
+        imdbid=response.data.imdb_id;
+    })
+    
+    
+    url=`https://www.omdbapi.com/`
+    await axios.get(url,{
+        params:{
+            apikey:"530d83d3",
+            i:imdbid
+        }
+    })
+    .then(function(response){
+        console.log(response);
+        document.getElementById('name').innerText=`${response.data.Title}`
+    })
+    window.addEventListener('hashchange', hashHandler1, true);
+}
+
+function hashHandler() {
+    trending();
+    //console.log("Called1")
+}
+function hashHandler1() {
+    //console.log("Called2")
+    if(string !=="")
+    search();
+    else
+    trending();
+    
 }
