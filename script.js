e167fe79a0ffeb7510ed5e630c28903245df3e13
Chirpatel <chirpatelss@gmail.com>
2020-05-10 22:54:00 +0530

    Basic

--- script.js
+++ script.js
@@ -99,16 +99,3 @@ function searchpage(page,string){
         }
     })
 }
-
-/*
-<div class="col-sm-6 col-md-4 col-lg-3 custom">
-            <div class="card" >
-                <img class="card-img-top" src="http://image.tmdb.org/t/p/w780//rciEbCqwOiHX688WWJgSpnP0Ls4.jpg" alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-</div>
-
-*/
